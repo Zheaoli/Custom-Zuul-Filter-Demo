5b9cb59ec0d0d893a36e0a78a672f3b522df20dc
manjusaka <me@manjusaka.me>
2020-06-18 11:45:00 +0800

    Custom Zuul Filter Demo

---
+++ src/main/java/me/manjusaka/zuuldemo/filters/CustomZuulFilter.java
@@ -0,0 +1,72 @@
+package me.manjusaka.zuuldemo.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+@Component
+public class CustomZuulFilter extends ZuulFilter {
+    /**
+     * to classify a filter by type. Standard types in Zuul are "pre" for pre-routing filtering,
+     * "route" for routing to an origin, "post" for post-routing filters, "error" for error handling.
+     * We also support a "static" type for static responses see  StaticResponseFilter.
+     * Any filterType made be created or added and run by calling FilterProcessor.runFilters(type)
+     *
+     * @return A String representing that type
+     */
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    /**
+     * filterOrder() must also be defined for a filter. Filters may have the same  filterOrder if precedence is not
+     * important for a filter. filterOrders do not need to be sequential.
+     *
+     * @return the int order of a filter
+     */
+    @Override
+    public int filterOrder() {
+        return PRE_DECORATION_FILTER_ORDER + 1;
+    }
+
+    /**
+     * a "true" return from this method means that the run() method should be invoked
+     *
+     * @return true if the run() method should be invoked. false will not invoke the run() method
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * if shouldFilter() is true, this method will be invoked. this method is the core method of a ZuulFilter
+     *
+     * @return Some arbitrary artifact may be returned. Current implementation ignores it.
+     * @throws ZuulException if an error occurs during execution.
+     */
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest httpServletRequest = ctx.getRequest();
+        String token = httpServletRequest.getParameter("token");
+        if (!"abc".equals(token)) {
+            ctx.setRouteHost(null);
+//            ctx.set(ZuulConstant.SERVICE_ID_KEY, null);
+            // Set fallback response.
+            ctx.setResponseBody("auth failed");
+            ctx.setResponseStatusCode(HttpStatus.SC_UNAUTHORIZED);
+            // Set Response ContentType
+            ctx.getResponse().setContentType("application/json; charset=utf-8");
+        }
+        return null;
+    }
+}
---
+++ src/test/java/me/manjusaka/zuuldemo/ZuulDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.manjusaka.zuuldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
